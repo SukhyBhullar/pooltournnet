c900f78355d85501010a16b67a40696d02a924f8
Sukhvinder Bhullar <beast698@hotmail.com>
2014-11-26 14:14:08 +0000

    Initial Commit

---
+++ PoolTourn/PoolTourn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoolTourn.Startup))]
+namespace PoolTourn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
